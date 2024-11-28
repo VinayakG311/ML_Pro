40cf01e2e5fde76cc60860480f8f0bce9f0e11a7
vinayak-77 <vinayak270603@gmail.com>
2024-11-29 02:15:49 +0530

    Frontend done

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
 
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask,request
+from flask import Flask,request,jsonify
+from flask_cors import CORS,cross_origin
 import sklearn
 import pickle
 import numpy as np
+import pandas as pd
 app = Flask(__name__)
+CORS(app,support_credentials=True)
 
 input_data = [
     0,                           # Gender
@@ -28,20 +31,56 @@ input_data = [
 ]
 
 # Convert to a 2D array (1 sample, n features)
-input_data = np.array([input_data])
-with open('/home/vinayakg311/Random_Forest_Optimised_model.pkl', 'rb') as f:
+gender = {'Female': 0, 'Male': 1}
+bmi_category = {'Normal': 0, 'Normal Weight': 1, 'Obese': 2, 'Overweight': 3}
+original_occupations = ['Accountant','Doctor','Engineer','Lawyer','Nurse','Salesperson','Teacher']
+
+occupation_columns = ['Occupation_Accountant','Occupation_Doctor','Occupation_Engineer','Occupation_Lawyer','Occupation_Nurse','Occupation_Salesperson','Occupation_Teacher']
+def pre_process(X):
+  X[['Systolic','Diastolic']] = X['Blood Pressure'].str.split('/',expand=True).apply(pd.to_numeric, errors='coerce')
+  # X = pd.get_dummies(X,columns=["Occupation"],prefix="Occupation")
+  X.drop('Blood Pressure',axis=1,inplace=True)
+  X["Gender"] = X["Gender"].map(gender)
+  X["BMI Category"] =  X["BMI Category"].map(bmi_category)
+  for col in occupation_columns:
+      if col not in X.columns:
+          X[col] = False
+  occupation = X['Occupation'].iloc[0]
+  occupation_column = f'Occupation_{occupation}' if occupation in original_occupations else None
+  if occupation_column:
+      X[occupation_column] = 1
+  X.drop('Occupation',axis=1,inplace=True)
+  numerical_features = ["Age","Sleep Duration","Quality of Sleep","Physical Activity Level","Stress Level","Heart Rate","Daily Steps","Diastolic","Systolic"]
+  X[numerical_features] = X[numerical_features].apply(pd.to_numeric,errors='coerce')
+  with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+  X[numerical_features] = scaler.transform(X[numerical_features])
+  return X
+  
+with open('SVM_Optimised_model.pkl', 'rb') as f:
   model = pickle.load(f)
 
-@app.route('/api/input', methods=['GET'])
-def GetPrediction():
-  inpt = request.args['query']
-  input_data = [float(i) for i in inpt.split(",")]
-  input_data = np.array([input_data])
-  otpt = model.predict(input_data)
-  d={}
-  d["output"]=otpt[0]
-  print(input)
-  return d
 
+  
+@app.route('/predict', methods=['POST','OPTIONS'])
+@cross_origin(supports_credentials=True)
+def GetPrediction():
+  input_data = request.json
+  # print(input_data)
+  df = pd.DataFrame([input_data])
+  # print(df)
+  df = pre_process(df)
+  
+  with open('SVM_Optimised_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+  print(df.columns)
+  pred = model.predict(df)
+  d={}
+  d["output"]=pred[0]
+  print(pred)
+  return jsonify(d)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 # print(model.predict(input_data))
 # app.run(debug=False,host="0.0.0.0",port=4444)
